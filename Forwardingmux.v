ec4cff98a1f6f209126f6cc613e6744100ef3bad
Naomi-ANN <nevellalex@gmail.com>
2026-05-05 17:51:52 -0700

    Update Forwardingmux.v

--- Forwardingmux.v
+++ Forwardingmux.v
@@ -2,10 +2,10 @@
 module Forwardingmux #(parameter WIDTH_inp_forward =  32) (
 
     input [1:0] sel_forward,
-    input [(WIDTH_inp-1):0] in_1_forward,
-    input [(WIDTH_inp-1):0] in_2_forward,
-    input [(WIDTH_inp-1):0] in_3_forward,
-    output [(WIDTH_inp-1):0] outp_forward
+    input [(WIDTH_inp_forward-1):0] in_1_forward,
+    input [(WIDTH_inp_forward-1):0] in_2_forward,
+    input [(WIDTH_inp_forward-1):0] in_3_forward,
+    output [(WIDTH_inp_forward-1):0] outp_forward
 );
 assign outp_forward = (sel_forward == 2'b10)? in_3_forward: (sel_forward == 2'b01) ? in_2_forward: in_1_forward;  
 endmodule
