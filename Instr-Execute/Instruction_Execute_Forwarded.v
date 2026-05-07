58908c5089e577e371c95f4c5666b3db6f5cb410
mariec-j <mcjardiniano@cpp.edu>
2026-05-07 02:44:48 -0700

    Added Modified Modules
    - Added Forwarding vers. of Instr_Exe
    - Added Forwarding vers. of the MIPS_Pipeline

---
+++ Instr-Execute/Instruction_Execute_Forwarded.v
@@ -0,0 +1,140 @@
+`timescale 1ns / 1ps
+
+
+module Instruction_Execute_Forwarded (
+// - - - - - - inputs - - - - - -
+    input [31:0]    NPC,
+    input [31:0]    ReadData1,
+    input [31:0]    ReadData2,
+    input           ALU_Src,
+    input [31:0]    SignExtend,
+    input [1:0]     ALU_Op,
+    input [4:0]     Instr_2016,
+    input [4:0]     Instr_1511,
+    input           RegDst,
+    input [1:0]     WB,
+    input [2:0]     Mem,
+    input           clk,
+    input           rst,
+/////////////////// NEW INPUTS
+input [4:0]     Rs,
+input [4:0]     EX_MEM_Rd,
+input [4:0]     MEM_WB_Rd,
+input           EX_MEM_RegWrite,
+input           MEM_WB_RegWrite,
+input [31:0]    EX_MEM_value,
+input [31:0]    MEM_WB_value,
+// - - - - - - outputs - - - - - -
+    output [1:0]    IE_WB,
+    output [2:0]    IE_Mem,
+    output [31:0]   Add_Result,
+    output          Zero,
+    output [31:0]   ALU_Result,
+    output [31:0]   ReadData2_ex_mem,
+    output [4:0]    muxOut_5bit
+    );
+
+//internal wires the 5 outputs of aadder, ALU, MUX, ALUcontrol, Mux2
+wire [31:0] adder_32_out; //output of adder
+wire [31:0] mux_32_out; //mux 32 bit output
+wire [31:0] ALU_out; // 32 bit output of alue
+wire [2:0] ALU_control_out;//output of ALU control
+wire [4:0] mux_5_out;//output of second mux
+wire ALU_Zero_out;
+/////////////////////////////////////////////////////
+wire [1:0] ForwardA;
+wire [1:0] ForwardB;
+wire [31:0] muxA_out;  
+wire [31:0] muxB_out;
+
+
+//Adder
+adder adder01 (
+    .inp_1(NPC),
+    .inp_2(SignExtend << 2),
+    .Add_Result(adder_32_out)
+);
+
+//ALU_control
+ALU_Control ALU_control01(
+    .ALU_Op(ALU_Op),
+    .SignExtend(SignExtend[5:0]),
+    .ALU_Control_outp(ALU_control_out)
+);
+
+//ALU
+ALU ALU01 (
+    .ReadData1(muxA_out), //a
+    .Data2(mux_32_out), //b
+    .control(ALU_control_out), 
+    .ALU_Result(ALU_out),
+    .zero_flag(ALU_Zero_out)
+);
+
+//Mux_32bit
+mux #(.WIDTH_inp(32)) mux_32bit (
+    .sel(ALU_Src),
+    .in_1(muxB_out),
+    .in_2(SignExtend),
+    .outp(mux_32_out)
+);
+
+//Mux_5bit
+mux #(.WIDTH_inp(5)) mux_5bit (
+    .sel(RegDst),
+    .in_1(Instr_2016),
+    .in_2(Instr_1511),
+    .outp(mux_5_out)
+);
+
+//Latch
+EX_Mem_Latch EX_M_Latch (
+// - - - - - - inputs - - - - - -
+    .WB(WB),
+    .Mem(Mem),
+    .Add_Result(adder_32_out), 
+    .Zero(ALU_Zero_out),
+    .ALU_Result(ALU_out),
+    .ReadData2_ex_mem(ReadData2),
+    .muxOut_5bit(mux_5_out),
+    .clk(clk),
+    .rst(rst),
+// - - - - - - outputs - - - - - -
+// Takes in all outputs and buffers for Memory stage
+    .EX_Mem_Latch_WB(IE_WB),
+    .EX_Mem_Latch_Mem(IE_Mem),
+    .EX_Mem_Latch_Add_Result(Add_Result),
+    .EX_Mem_Latch_Zero(Zero),
+    .EX_Mem_Latch_ALU_Result(ALU_Result),
+    .EX_Mem_Latch_ReadData2_ex_mem(ReadData2_ex_mem),
+    .EX_Mem_Latch_muxOut_5bit(muxOut_5bit)
+);
+
+//Forwarding.v 
+
+Forwarding u_Forwarding(
+    .ID_EX_Rs          (Rs              ),
+    .ID_EX_Rt          (Instr_2016      ),
+    .EX_MEM_RegisterRd (EX_MEM_Rd       ),
+    .MEM_WB_RegisterRd (MEM_WB_Rd       ),
+    .EX_MEM_RegWrite   (EX_MEM_RegWrite ),
+    .MEM_WB_RegWrite   (MEM_WB_RegWrite ),
+    .ForwardA          (ForwardA        ),
+    .ForwardB          (ForwardB        )
+);
+Forwardingmux #(.WIDTH_inp_forward(32)) muxA (
+    .sel_forward  (ForwardA),
+    .in_1_forward (ReadData1),
+    .in_2_forward (MEM_WB_value),
+    .in_3_forward (EX_MEM_value),
+    .outp_forward (muxA_out)
+);
+
+Forwardingmux #(.WIDTH_inp_forward(32)) muxB (
+    .sel_forward  (ForwardB),
+    .in_1_forward (ReadData2),
+    .in_2_forward (MEM_WB_value),
+    .in_3_forward (EX_MEM_value),
+    .outp_forward (muxB_out)
+);
+endmodule
---
+++ MIPS_Pipeline_Fowarded.v
@@ -0,0 +1,150 @@
+`timescale 1ns / 1ps
+
+module MIPS_Pipeline_Fowarded (
+input clk,
+input rst    
+); 
+
+//Fetch.v wires
+wire [31:0] if_id_instr, if_id_npc;
+
+//InstructionDecode.v wires
+wire [1:0]  InstructionDecode_WB;
+wire [2:0]  InstructionDecode_M;
+wire [1:0]  InstructionDecode_ALUOp;
+wire        InstructionDecode_ALUSrc;
+wire        InstructionDecode_RegDst;
+wire [31:0] InstructionDecode_NPC;
+wire [31:0] InstructionDecode_ReadData1;
+wire [31:0] InstructionDecode_ReadData2;
+wire [31:0] InstructionDecode_SignExtend;
+wire [4:0]  InstructionDecode_Instr_2016;
+wire [4:0]  InstructionDecode_Instr_1511;
+    
+//Instruction_Execute.v wires
+wire [1:0]    IE_WB;
+wire [2:0]    IE_Mem;
+wire [31:0]   Add_Result;
+wire          Zero;
+wire [31:0]   ALU_Result;
+wire [31:0]   ReadData2_ex_mem;
+wire [4:0]    muxOut_5bit;
+
+wire        w_Branch;
+wire        w_MemRead;
+wire        w_MemWrite;
+
+//Instruction_Mem_Wb.v wires
+wire        RegWrite;
+wire        MemtoReg;
+wire [31:0] ReadData;
+wire [31:0] Mem_ALU_Result;
+wire [4:0]  MemWriteReg;
+wire        PCSrc;
+wire        M_RegWrite;
+wire [4:0]  M_MemWriteReg;
+
+//Instruction_WB.v wires
+wire [31:0] WriteData;
+
+// - - - - - - Instruction Fetch
+instrFetch u_instrFetch(
+// Inputs - - - - - - - - - - - - -
+    .clk           	(clk            ),
+    .rst           	(rst            ),
+    .ex_mem_pc_src 	(PCSrc          ),//input from Instruction_Mem_Wb.v
+    .ex_mem_npc    	(Add_Result     ),//input from Instruction_Execute.v
+// Outputs - - - - - - - - - - - - -
+    .if_id_instr   	(if_id_instr    ),
+    .if_id_npc     	(if_id_npc      )
+);
+
+// - - - - - - Instruction Decode
+InstructionDecode u_InstructionDecode(
+// Inputs - - - - - - - - - - - - -
+    .clk                          	(clk                     ),
+    .rst                          	(rst                     ),
+    .RegWrite                     	(RegWrite                ),
+    .IFID_instruction             	(if_id_instr             ),//fetch.v
+    .IFID_NPC                     	(if_id_npc               ),//fetch.v
+    .MemWBLatch_WriteReg          	(MemWriteReg             ),//instruction_Mem_Wb.v
+    .WBMux_WriteData              	(WriteData               ),//instruction_Mem_Wb.v
+// Outputs - - - - - - - - - - - - -
+    .InstructionDecode_WB         	(InstructionDecode_WB          ),
+    .InstructionDecode_M          	(InstructionDecode_M           ),
+    .InstructionDecode_ALUOp      	(InstructionDecode_ALUOp       ),
+    .InstructionDecode_ALUSrc     	(InstructionDecode_ALUSrc      ),
+    .InstructionDecode_RegDst     	(InstructionDecode_RegDst      ),
+    .InstructionDecode_NPC        	(InstructionDecode_NPC         ),
+    .InstructionDecode_ReadData1  	(InstructionDecode_ReadData1   ),
+    .InstructionDecode_ReadData2  	(InstructionDecode_ReadData2   ),
+    .InstructionDecode_SignExtend 	(InstructionDecode_SignExtend  ),
+    .InstructionDecode_Instr_2016 	(InstructionDecode_Instr_2016  ),
+    .InstructionDecode_Instr_1511 	(InstructionDecode_Instr_1511  )
+);
+
+//  - - - - - - Instruction Execution
+Instruction_Execute u_Instruction_Execute(
+// Inputs - - - - - - - - - - - - -
+    .NPC              	(InstructionDecode_NPC               ),
+    .ReadData1        	(InstructionDecode_ReadData1         ),
+    .ReadData2        	(InstructionDecode_ReadData2         ),
+    .ALU_Src          	(InstructionDecode_ALUSrc            ),
+    .SignExtend       	(InstructionDecode_SignExtend        ),
+    .ALU_Op           	(InstructionDecode_ALUOp             ),
+    .Instr_2016       	(InstructionDecode_Instr_2016        ),
+    .Instr_1511       	(InstructionDecode_Instr_1511        ),
+    .RegDst           	(InstructionDecode_RegDst            ),
+    .WB               	(InstructionDecode_WB                ),
+    .Mem              	(InstructionDecode_M                 ),
+    .clk              	(clk               ),
+    .rst              	(rst               ),
+// Outputs - - - - - - - - - - - - -
+    .IE_WB            	(IE_WB             ),
+    .IE_Mem           	(IE_Mem            ),
+    .Add_Result       	(Add_Result        ),
+    .Zero             	(Zero              ),
+    .ALU_Result       	(ALU_Result        ),
+    .ReadData2_ex_mem 	(ReadData2_ex_mem  ),
+    .muxOut_5bit      	(muxOut_5bit       )
+);
+
+assign w_Branch   = IE_Mem[2];
+assign w_MemRead  = IE_Mem[1];
+assign w_MemWrite = IE_Mem[0];
+
+//  - - - - - - Instruction Memory
+Instruction_Mem u_Instruction_Mem(
+// Inputs - - - - - - - - - - - - -
+    .WB               	(IE_WB             ),
+    .M_ctlout         	(w_Branch          ),
+    .Zero             	(Zero              ),
+    .MemWrite         	(w_MemWrite        ),
+    .ALU_Result       	(ALU_Result        ),
+    .ReadData2_ex_mem 	(ReadData2_ex_mem  ),
+    .MemRead          	(w_MemRead         ),
+    .M_muxOut_5bit      (muxOut_5bit       ),
+    .clk              	(clk               ),
+    .rst              	(rst               ),
+// Outputs - - - - - - - - - - - - -
+    .M_RegWrite         (M_RegWrite          ),
+    .MemtoReg         	(MemtoReg          ),
+    .ReadData         	(ReadData          ),
+    .Mem_ALU_Result   	(Mem_ALU_Result    ),
+    .M_MemWriteReg      (M_MemWriteReg       ),
+    .PCSrc            	(PCSrc             )
+);
+
+//  - - - - - -  Instruction Write Back
+Instr_WB u_Instr_WB(
+    .MemtoReg       (MemtoReg),
+    .ReadData       (ReadData),
+    .M_RegWrite     (M_RegWrite),
+    .M_MemWriteReg  (M_MemWriteReg),
+    .Mem_ALU_Result (Mem_ALU_Result),
+    .WriteData      (WriteData),
+    .RegWrite       (RegWrite),
+    .MemWriteReg    (MemWriteReg)
+);
+    
+endmodule
